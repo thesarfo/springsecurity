2e0362506df386ed4524eebcc11b3e43c7697f4c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 14:28:43 +0000

    map admin register dto to admin entity

---
+++ src/main/java/dev/thesarfo/springsecurity/model/mapper/AdminRegisterRequestToAdminEntityMapper.java
@@ -0,0 +1,27 @@
+package dev.thesarfo.springsecurity.model.mapper;
+
+import dev.thesarfo.springsecurity.model.dto.request.AdminRegisterRequest;
+import dev.thesarfo.springsecurity.model.entity.admin.AdminEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AdminRegisterRequestToAdminEntityMapper extends BaseMapper<AdminRegisterRequest, AdminEntity> {
+
+
+    @Named("mapForSaving")
+    default AdminEntity mapForSaving(AdminRegisterRequest adminRegisterRequest) {
+        return AdminEntity.builder()
+                .email(adminRegisterRequest.getEmail())
+                .firstName(adminRegisterRequest.getFirstName())
+                .lastName(adminRegisterRequest.getLastName())
+                .phoneNumber(adminRegisterRequest.getPhoneNumber())
+                .build();
+    }
+
+    static AdminRegisterRequestToAdminEntityMapper initialize() {
+        return Mappers.getMapper(AdminRegisterRequestToAdminEntityMapper.class);
+    }
+
+}
