bd9a9dbf2ab6140161678af32d5052cb24b4cff9
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 13:50:16 +0000

    throw exception for invalid passwords

---
+++ src/main/java/dev/thesarfo/springsecurity/exception/auth/PasswordNotValidException.java
@@ -0,0 +1,22 @@
+package dev.thesarfo.springsecurity.exception.auth;
+
+import java.io.Serial;
+
+public class PasswordNotValidException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 7389659106153108528L;
+
+    private static final String DEFAULT_MESSAGE = """
+            Password is not valid!
+            """;
+
+    public PasswordNotValidException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public PasswordNotValidException(final String message) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+
+}
