3032f5641487e2b8f2ddc2f08471f4f6cae42c6d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 15:19:26 +0000

    add dto for creating products

---
+++ src/main/java/dev/thesarfo/springsecurity/model/dto/request/ProductCreateRequest.java
@@ -0,0 +1,38 @@
+package dev.thesarfo.springsecurity.model.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductCreateRequest {
+
+    @Size(
+            min = 1,
+            message = "Product name can't be blank."
+    )
+    private String name;
+
+    @DecimalMin(
+            value = "0.0001",
+            message = "Amount must be bigger than 0"
+    )
+    private BigDecimal amount;
+
+    @DecimalMin(
+            value = "0.0001",
+            message = "Unit Price must be bigger than 0"
+    )
+    private BigDecimal unitPrice;
+}
+
