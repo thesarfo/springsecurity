bc36c5e5ee700c5c8909706bc048cf7cbc6a0616
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 15:19:58 +0000

    add dto for pagingating products

---
+++ src/main/java/dev/thesarfo/springsecurity/model/dto/request/ProductPagingRequest.java
@@ -0,0 +1,15 @@
+package dev.thesarfo.springsecurity.model.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class ProductPagingRequest extends CustomPagingRequest {
+
+
+}
