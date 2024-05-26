92cff14e3c8693395cd59d4322ffdde91ef56c9e
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 14:27:41 +0000

    add admin entity

---
+++ src/main/java/dev/thesarfo/springsecurity/model/entity/admin/AdminEntity.java
@@ -0,0 +1,83 @@
+package dev.thesarfo.springsecurity.model.entity.admin;
+
+import dev.thesarfo.springsecurity.model.entity.BaseEntity;
+import dev.thesarfo.springsecurity.model.enums.TokenClaims;
+import dev.thesarfo.springsecurity.model.enums.UserStatus;
+import dev.thesarfo.springsecurity.model.enums.UserType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CASE_ADMIN")
+public class AdminEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(
+            name = "PHONE_NUMBER",
+            length = 20
+    )
+    private String phoneNumber;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private UserType userType = UserType.ADMIN;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus = UserStatus.ACTIVE;
+
+    public Map<String, Object> getClaims() {
+
+        final Map<String, Object> claims = new HashMap<>();
+
+        claims.put(TokenClaims.USER_ID.getValue(), this.id);
+        claims.put(TokenClaims.USER_TYPE.getValue(), this.userType);
+        claims.put(TokenClaims.USER_STATUS.getValue(), this.userStatus);
+        claims.put(TokenClaims.USER_FIRST_NAME.getValue(), this.firstName);
+        claims.put(TokenClaims.USER_LAST_NAME.getValue(), this.lastName);
+        claims.put(TokenClaims.USER_EMAIL.getValue(), this.email);
+        claims.put(TokenClaims.USER_PHONE_NUMBER.getValue(), this.phoneNumber);
+
+        return claims;
+
+    }
+
+}
