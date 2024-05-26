9a821623c50248ff04a28d635018497e6f9d431d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 15:27:56 +0000

    implement product deletion

---
+++ src/main/java/dev/thesarfo/springsecurity/service/impl/product/ProductDeleteServiceImpl.java
@@ -0,0 +1,27 @@
+package dev.thesarfo.springsecurity.service.impl.product;
+
+import dev.thesarfo.springsecurity.exception.product.ProductNotFoundException;
+import dev.thesarfo.springsecurity.model.entity.ProductEntity;
+import dev.thesarfo.springsecurity.repository.product.ProductRepository;
+import dev.thesarfo.springsecurity.service.product.ProductDeleteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductDeleteServiceImpl implements ProductDeleteService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public void deleteProductById(String productId) {
+
+        final ProductEntity productEntityToBeDelete = productRepository
+                .findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException("With given productID = " + productId));
+
+        productRepository.delete(productEntityToBeDelete);
+
+    }
+
+}
