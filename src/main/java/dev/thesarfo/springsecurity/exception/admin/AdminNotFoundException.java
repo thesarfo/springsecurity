8bbf47850188ba8bbbc6d7eb8b109cb6e1eaf2d9
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 14:26:06 +0000

    throw exceptions for already existing and not found admins

---
+++ src/main/java/dev/thesarfo/springsecurity/exception/admin/AdminNotFoundException.java
@@ -0,0 +1,22 @@
+package dev.thesarfo.springsecurity.exception.admin;
+
+import java.io.Serial;
+
+public class AdminNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -6226206922525682121L;
+
+    private static final String DEFAULT_MESSAGE = """
+            Admin not found!
+            """;
+
+    public AdminNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public AdminNotFoundException(final String message) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+
+}
