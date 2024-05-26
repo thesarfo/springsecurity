ece73dbd1fa70466696eb89c43dc644809cdc3be
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 10:14:07 +0000

    add base and token entity

---
+++ src/main/java/dev/thesarfo/springsecurity/model/entity/InvalidTokenEntity.java
@@ -0,0 +1,25 @@
+package dev.thesarfo.springsecurity.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "INVALID_TOKEN")
+public class InvalidTokenEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "TOKEN_ID")
+    private String tokenId;
+
+}
