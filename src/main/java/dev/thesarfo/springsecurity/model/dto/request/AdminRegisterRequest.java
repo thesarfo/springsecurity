e81f3f46ec3b2afbcf59ccb95f4657875714d530
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 14:27:13 +0000

    implement dto for admin registration requests

---
+++ src/main/java/dev/thesarfo/springsecurity/model/dto/request/AdminRegisterRequest.java
@@ -0,0 +1,36 @@
+package dev.thesarfo.springsecurity.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminRegisterRequest {
+
+    @Email(message = "Please enter valid e-mail address")
+    @Size(min = 7, message = "Minimum e-mail length is 7 characters.")
+    private String email;
+
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank(message = "First name can't be blank.")
+    private String firstName;
+
+    @NotBlank(message = "Last name can't be blank.")
+    private String lastName;
+
+    @NotBlank(message = "Phone number can't be blank.")
+    @Size(min = 11, max = 20)
+    private String phoneNumber;
+
+}
