0691aea8d7b58e842e433945c89d906bcfb4070d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 16:43:20 +0000

    add base config for testing

---
+++ src/test/java/dev/thesarfo/springsecurity/base/AbstractRestControllerTest.java
@@ -0,0 +1,96 @@
+package dev.thesarfo.springsecurity.base;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.thesarfo.springsecurity.builder.AdminEntityBuilder;
+import dev.thesarfo.springsecurity.builder.UserEntityBuilder;
+import dev.thesarfo.springsecurity.config.TokenConfigurationParameter;
+import dev.thesarfo.springsecurity.model.Token;
+import dev.thesarfo.springsecurity.model.enums.TokenClaims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AbstractRestControllerTest extends AbstractTestContainerConfiguration {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected Token mockAdminToken;
+
+    protected Token mockUserToken;
+
+    @Mock
+    private TokenConfigurationParameter tokenConfiguration;
+
+
+    @BeforeEach
+    public void initializeAuth() {
+
+        this.tokenConfiguration = new TokenConfigurationParameter();
+        this.mockAdminToken = this.generate(new AdminEntityBuilder().withValidFields().build().getClaims());
+        this.mockUserToken = this.generate(new UserEntityBuilder().withValidFields().build().getClaims());
+
+    }
+
+    private Token generate(Map<String, Object> claims) {
+
+        final long currentTimeMillis = System.currentTimeMillis();
+
+        final Date tokenIssuedAt = new Date(currentTimeMillis);
+
+        final Date accessTokenExpiresAt = DateUtils.addMinutes(new Date(currentTimeMillis), tokenConfiguration.getAccessTokenExpireMinute());
+
+        final String accessToken = Jwts.builder()
+                .header()
+                .add(TokenClaims.TYP.getValue(), OAuth2AccessToken.TokenType.BEARER.getValue())
+                .and()
+                .id(UUID.randomUUID().toString())
+                .issuer(tokenConfiguration.getIssuer())
+                .issuedAt(tokenIssuedAt)
+                .expiration(accessTokenExpiresAt)
+                .signWith(tokenConfiguration.getPrivateKey())
+                .claims(claims)
+                .compact();
+
+        final Date refreshTokenExpiresAt = DateUtils.addDays(new Date(currentTimeMillis), tokenConfiguration.getRefreshTokenExpireDay());
+
+        final JwtBuilder refreshTokenBuilder = Jwts.builder();
+
+        final String refreshToken = refreshTokenBuilder
+                .header()
+                .add(TokenClaims.TYP.getValue(), OAuth2AccessToken.TokenType.BEARER.getValue())
+                .and()
+                .id(UUID.randomUUID().toString())
+                .issuer(tokenConfiguration.getIssuer())
+                .issuedAt(tokenIssuedAt)
+                .expiration(refreshTokenExpiresAt)
+                .signWith(tokenConfiguration.getPrivateKey())
+                .claim(TokenClaims.USER_ID.getValue(), claims.get(TokenClaims.USER_ID.getValue()))
+                .compact();
+
+        return Token.builder()
+                .accessToken(accessToken)
+                .accessTokenExpiresAt(accessTokenExpiresAt.toInstant().getEpochSecond())
+                .refreshToken(refreshToken)
+                .build();
+
+    }
+
+}
+
