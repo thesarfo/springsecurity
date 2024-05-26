7c2cedc1e8c65ec188f669e430659c5f04e20a3a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 13:06:24 +0000

    add method to invalidate jwt token on logout

---
+++ src/main/java/dev/thesarfo/springsecurity/model/dto/request/TokenInvalidateRequest.java
@@ -0,0 +1,19 @@
+package dev.thesarfo.springsecurity.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenInvalidateRequest {
+
+    @NotBlank
+    private String accessToken;
+
+    @NotBlank
+    private String refreshToken;
+
+}
