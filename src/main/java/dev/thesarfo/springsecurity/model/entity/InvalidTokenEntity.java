eb1ff9d1e585ef6b09d27f01bffa202bcebf780c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 14:24:58 +0000

    optimize imports

--- src/main/java/dev/thesarfo/springsecurity/model/entity/InvalidTokenEntity.java
+++ src/main/java/dev/thesarfo/springsecurity/model/entity/InvalidTokenEntity.java
@@ -1,7 +1,16 @@
 package dev.thesarfo.springsecurity.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
