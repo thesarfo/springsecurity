a7665c94512fb9c0499fc4057a82e4f780e3185d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 13:03:33 +0000

    user request dtos for signup and login

---
+++ src/main/java/dev/thesarfo/springsecurity/model/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package dev.thesarfo.springsecurity.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LoginRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
+
