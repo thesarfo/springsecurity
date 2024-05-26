70ca8338886fce91e7e95beba20adcae287e691d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 13:51:00 +0000

    throw exception for invalid, not found and already existing users

---
+++ src/main/java/dev/thesarfo/springsecurity/exception/user/UserNotFoundException.java
@@ -0,0 +1,22 @@
+package dev.thesarfo.springsecurity.exception.user;
+
+import java.io.Serial;
+
+public class UserNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -3952215105519401565L;
+
+    private static final String DEFAULT_MESSAGE = """
+            User not found!
+            """;
+
+    public UserNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public UserNotFoundException(final String message) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+
+}
