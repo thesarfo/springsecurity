03214ed09e6278320b8eac68ab071a4277c103da
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 09:04:30 +0000

    first commit

---
+++ src/main/java/dev/thesarfo/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.thesarfo.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
