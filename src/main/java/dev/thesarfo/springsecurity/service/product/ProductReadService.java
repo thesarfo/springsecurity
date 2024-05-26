d93f7c48e41c3184aa61cc5741c2b636949d07a7
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-26 15:26:17 +0000

    read product

---
+++ src/main/java/dev/thesarfo/springsecurity/service/product/ProductReadService.java
@@ -0,0 +1,13 @@
+package dev.thesarfo.springsecurity.service.product;
+
+import dev.thesarfo.springsecurity.model.Product;
+import dev.thesarfo.springsecurity.model.dto.request.ProductPagingRequest;
+import dev.thesarfo.springsecurity.model.dto.response.CustomPage;
+
+public interface ProductReadService {
+
+    Product getProductById(final String productId);
+
+    CustomPage<Product> getProducts(final ProductPagingRequest productPagingRequest);
+
+}
